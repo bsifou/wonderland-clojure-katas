bd46da2e6889d879f47b518b55f1247df9dd9a1b
Sifu Biri <sifeddinebiri@gmail.com>
2017-02-21 16:17:47 -0600

    solved the number finder prob

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,25 @@
 (ns wonderland-number.finder)
 
+(def start 100000)
+
+(def end 1000000)
+
+(def test-nums [2 3 4 5 6])
+
+(defn hold-test [n t]
+  (= (set (str n)) (set (str (* n t)))))
+
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (drop-while
+        #(not-every? (partial hold-test %) test-nums)
+        (range start end))))
+
+
+
+
+
+
+
+
+
